9fb5e480db1dada0f736b518fd547a7e4d4820c8
s21ifrahsaleem <ifrahsaleem@sabanciuniv.edu>
2021-04-23 17:07:56 +0500

    notifications page

--- socialapp310/lib/main.dart
+++ socialapp310/lib/main.dart
@@ -1,22 +1,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:socialapp310/routes/finishsignup.dart';
+import 'package:socialapp310/routes/notifications/notifications.dart';
 import 'package:socialapp310/routes/signup.dart';
 import 'package:socialapp310/routes/splashpage.dart';
 import 'package:socialapp310/routes/login.dart';
 import 'package:socialapp310/routes/welcome.dart';
-import 'package:socialapp310/routes/notifications.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   routes: {
-    // '/': (context) => SplashScreen(),
-    // '/welcome': (context) => Welcome(),
-    // '/login': (context) => Login(),
-    // '/signup': (context) => SignUp(),
-    // '/signupfinish': (context) => FinishSignupPage(),
-
-    '/': (context) => Notifications(),
+    '/': (context) => SplashScreen(),
+    '/welcome': (context) => Welcome(),
+    '/login': (context) => Login(),
+    '/signup': (context) => SignUp(),
+    '/signupfinish': (context) => FinishSignupPage(),
+    '/notifications': (context) => ActivityScreen(),
     // '/signup': (context) => SignUp(),
 
   } ,
