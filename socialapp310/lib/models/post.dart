d278a2838444d4259b85930b45d74ed22c906f68
Jeren Annagurbanova <67991797+Jeren-A@users.noreply.github.com>
2021-05-08 10:52:42 +0300

    no welcome page

--- socialapp310/lib/models/post.dart
+++ socialapp310/lib/models/post.dart
@@ -16,15 +16,15 @@ class Post {
 }
 location x = location(loc_name: "Some Location", longitude: 41, latitude: 28.9);
 
-Post post = Post(username: "Generic Name",loc: x, likes: 100, ImageUrlAvatar: "assets/Dog/cutegolden.jpg", reposts: 20,
+Post post = Post(username: "Some Name",loc: x, likes: 100, ImageUrlAvatar: "assets/Dog/cutegolden.jpg", reposts: 20,
     ImageUrlPost: "assets/Dog/index.jpg", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 100);
 List<Post> posts = [Post(username: "Generic Name",loc: x, likes: 15, ImageUrlAvatar: "assets/Dog/another.jpg", reposts: 20,
     ImageUrlPost: "assets/Dog/scenicgolden.jpg", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 20),
-  Post(username: "Generic Name",loc: x, likes: 100, ImageUrlAvatar: "assets/Dog/last.jpg", reposts: 58,
+  Post(username: "other Name",loc: x, likes: 100, ImageUrlAvatar: "assets/Dog/last.jpg", reposts: 58,
       ImageUrlPost: "assets/Dog/doglifting.png", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 30),
   Post(username: "Generic Name",loc: x, likes: 235, ImageUrlAvatar: "assets/Dog/cutegolden.jpg", reposts: 62,
       ImageUrlPost: "assets/Dog/dogmask.jpg", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 100),
   Post(username: "Generic Name",loc: x, likes: 1880, ImageUrlAvatar: "assets/Dog/cuteshiba.png", reposts: 108,
       ImageUrlPost: "assets/Dog/hungrydog.jpg", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 2),
   Post(username: "Generic Name",loc: x, likes: 10000, ImageUrlAvatar: "assets/Dog/cutepug.jpg", reposts: 50,
-      ImageUrlPost: "assets/Dog/husky.jpg", caption: "This is a generic sentence.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 0)];
+      ImageUrlPost: "assets/Dog/husky.jpg", caption: "caption.This is a generic sentence.This is a generic sentence.", date: "April 17 2021", comments: 0)];
